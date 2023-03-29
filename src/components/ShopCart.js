0d445f084d935ebf690809e263d5720a6de6488f
Soraya Mahmoudi <Soraya_mahmoudi95@yahoo.com>
2023-03-29 07:31:43 +0430

    added Navbar

---
+++ src/components/ShopCart.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ShopCart=()=>{
+    return(
+        <div>ShopCart</div>
+    )
+}
+export default ShopCart;
--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import { Link } from 'react-router-dom';
 
+//Context
+import { CartContext } from '../../context/CartContextProvider';
+
 //Functions
-import { shorten } from '../../helpers/functions';
+import { isInCart, shorten, quantityCount } from '../../helpers/functions';
+
+//ICONS
+import trashIcon from '../../assets/icons/trash.svg';
+
 const Product=({productData})=>{
+    const {state, dispatch}=useContext(CartContext);
   return(
     <div>
         <img src={productData.image} alt="product" style={{width:"200px"}}/>
@@ -12,7 +20,14 @@ const Product=({productData})=>{
         <div>
             <Link to={`/products/${productData.id}`}>Details</Link>
             <div>
-                <button>Add to cart</button>
+                {quantityCount(state, productData.id)=== 1 && <button onClick={()=>dispatch({type:"REMOVE_ITEM", payload:productData})}>-</button>}
+                {quantityCount(state, productData.id)> 1 && <button onClick={()=>dispatch({type:"DECREASE", payload:productData})}><img src={trashIcon} alt="trash" style={{width:"20px"}}/></button>}
+                {
+                    isInCart(state, productData.id)?
+                    <button onClick={()=>dispatch({type:"INCREASE", payload: productData})}>+</button>:
+                    <button onClick={()=>dispatch({type:"ADD_ITEM", payload: productData})}>Add to Cart</button>
+                }
+               
             </div>
         </div>
     </div>
