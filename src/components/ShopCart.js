195d3d06232f971b6a140b4498cf57dfa48cb714
Soraya Mahmoudi <Soraya_mahmoudi95@yahoo.com>
2023-03-30 06:38:26 +0430

    added cart.css and debugging

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React,{useContext} from "react";
+import { Link } from "react-router-dom";
+
+//Components
+import Cart from './shared/Cart'
+
+//Context
+import { CartContext } from "../context/CartContextProvider";
 
 const ShopCart=()=>{
+   const {state, dispatch}= useContext(CartContext);
+
     return(
-        <div>ShopCart</div>
+        <div>
+            <div>
+                {state.selectedItems.map(item=> <Cart key={item.id} data={item}/>)}
+            </div>
+            {
+                state.itemsCounter > 0 && <div>
+                    <p><span>Total Items:</span>{state.itemsCounter}</p>
+                    <p><span>Total Payments:</span>{state.total} $</p>
+                    <div>
+                        <button onClick={()=>dispatch({type:"CLEAR"})}>Clear</button>
+                        <button onClick={()=>dispatch({type:"CHECKOUT"})}>Checkout</button>
+                    </div>
+                </div>
+            }
+            {
+                state.itemsCounter===0 && !state.checkout && <div>
+                    <h3>Want to buy?</h3>
+                    <Link to="/products">Go to shop</Link>
+                </div>
+            }
+            {
+                state.checkout && <div>
+                    <h3>Checked out successfully</h3>
+                    <Link to="/products">Buy More</Link>
+                </div>
+            }
+        </div>
     )
 }
 export default ShopCart;
